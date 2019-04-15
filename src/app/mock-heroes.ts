20fda863fb6c650181f72433fe4aa001d6e37c5c
tiboitel <jules.boitelle@gmail.com>
2019-04-15 20:34:05 +0200

    Adding the chapters 4 of the angular course 'Tour of Heroes'

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from '../hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Bumble-Bee' },
+    { id: 12, name: 'Brolahla' },
+    { id: 13, name: 'Anguelos' },
+    { id: 14, name: 'Mary-rhoa' },
+    { id: 15, name: 'Jalo' },
+    { id: 16, name: 'Mega-Meca Man' },
+    { id: 17, name: 'Itioryu' },
+    { id: 18, name: 'Dr Slumb' },
+    { id: 19, name: 'Magemana' },
+    { id: 20, name: 'Vortex' }
+];
